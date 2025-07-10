909ce907e3aa012af576e726a2b80f3cf2cd55dc
Sebastian <jsebastianwrc@gmail.com>
2025-07-09 21:53:05 -0500

    feat: add deployment script and Vite configuration for GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/nombre-de-tu-repositorio/", // Cambia esto por el nombre real de tu repo
+  build: {
+    outDir: "dist",
+  },
+});
