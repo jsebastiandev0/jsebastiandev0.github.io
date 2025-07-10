1371c260eb7fd9f76d67ef03058cf84bf0b6333c
Sebastian <jsebastianwrc@gmail.com>
2025-07-09 23:23:59 -0500

    feat: enhance index and template with test for tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
+//import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 import templateHtml from "./template.html?raw";
 
 // Reemplazar placeholders con las rutas reales
-const html = templateHtml
-  .replace("VITE_LOGO_PLACEHOLDER", viteLogo)
-  .replace("JS_LOGO_PLACEHOLDER", javascriptLogo);
+const html = templateHtml.replace("VITE_LOGO_PLACEHOLDER", viteLogo);
+//.replace("JS_LOGO_PLACEHOLDER", javascriptLogo);
 
 document.querySelector("#app").innerHTML = html;
 
