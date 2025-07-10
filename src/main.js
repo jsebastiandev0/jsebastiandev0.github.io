f37bfda9fa7b9354dd4e44d16b7877032da25600
Sebastian <jsebastianwrc@gmail.com>
2025-07-10 01:47:25 -0500

    feat: restructure website layout with header, footer, and main content templates

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import "./style.css";
-//import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
-import templateHtml from "./template.html?raw";
 
-// Reemplazar placeholders con las rutas reales
-const html = templateHtml.replace("VITE_LOGO_PLACEHOLDER", viteLogo);
-//.replace("JS_LOGO_PLACEHOLDER", javascriptLogo);
+// Importar templates
+import mainTemplate from "./templates/main.html?raw";
+import headerTemplate from "./templates/header.html?raw";
+import footerTemplate from "./templates/footer.html?raw";
 
-document.querySelector("#app").innerHTML = html;
+// Procesar cada template individualmente
+const mainHtml = mainTemplate.replace("VITE_LOGO_PLACEHOLDER", viteLogo);
+const headerHtml = headerTemplate.replace("VITE_LOGO_PLACEHOLDER", viteLogo);
+const footerHtml = footerTemplate.replace(
+  "YEAR_PLACEHOLDER",
+  new Date().getFullYear()
+);
+
+// Usar los templates
+document.querySelector("#app").innerHTML = mainHtml;
+document.querySelector("#header").innerHTML = headerHtml;
+document.querySelector("#footer").innerHTML = footerHtml;
 
 setupCounter(document.querySelector("#counter"));
